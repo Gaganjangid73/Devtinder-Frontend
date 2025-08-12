4203679d1698b3ac0cc0c28606d24edf4ae2f64b
gaganjangid73 <gaganjangid02@gmail.com>
2025-08-12 15:01:57 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./Componet/Navbar";
 
 function App() {
   return (
     <>
-   <h1 className="text-3xl text-center text-orange-400 font-bold">Hello World </h1>
+    <Navbar/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
