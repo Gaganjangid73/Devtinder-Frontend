31c3d71401e6cff8bf1a71180155b95a489f9706
gaganjangid73 <gaganjangid02@gmail.com>
2025-08-17 11:38:31 +0530

    update app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Body from "./Componet/Body";
-import Login from "./Componet/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 import { Provider } from "react-redux";
-import AppStore from "./Utils/AppStore";
-import Feed from "./Componet/Feed";
-import Profile from "./Componet/profile";
-
+import appStore from "./Utils/appStore";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
+import Premium from "./components/Premium";
+import Chat from "./components/Chat";
 
 function App() {
   return (
-    <Provider store={AppStore}>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route index element={<Navigate to="feed" replace />} />
-            <Route path="login" element={<Login />} />
-            <Route path="feed" element={<Feed />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="signup" element={<div>Signup</div>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+              <Route path="/premium" element={<Premium />} />
+              <Route path="/chat/:targetUserId" element={<Chat />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 }
 
